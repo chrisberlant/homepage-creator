cf1ec4b92d71e85380140d934cef1a20752ab779
Chris Berlant <chris.berlant@gmail.com>
2024-10-22 23:33:44 +0200

    fix: Favicon not showing a broken image when loading fails

--- components/Favicon.tsx
+++ components/Favicon.tsx
@@ -1,22 +1,31 @@
 import Image from 'next/image';
 import FaviconNotFound from './FaviconNotFound';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Favicon({ url }: { url: string }) {
 	const [imageError, setimageError] = useState(false);
+	const [firstRender, setFirstRender] = useState(true);
 
-	return imageError ? (
-		<FaviconNotFound className='mr-2' />
-	) : (
-		<Image
-			className='mr-2'
-			height={15}
-			width={15}
-			src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${url}&sz=15`}
-			alt='favicon'
-			onError={() => setimageError(true)}
-		/>
+	useEffect(() => {
+		setFirstRender(false);
+	}, []);
+
+	return (
+		<>
+			{(firstRender || imageError) && (
+				<FaviconNotFound className='mr-2' />
+			)}
+
+			{!imageError && !firstRender && (
+				<Image
+					className='mr-2'
+					height={15}
+					width={15}
+					src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${url}&sz=15`}
+					alt='favicon'
+					onError={() => setimageError(true)}
+				/>
+			)}
+		</>
 	);
-}
-{
 }
