1f8136ed93b5d9282f39a0820cfdfdb8185014a3
Chris Berlant <chris.berlant@gmail.com>
2024-10-27 00:13:05 +0200

    refactor: LinkCardOverlay uses Favicon component

--- components/LinkCardOverlay.tsx
+++ components/LinkCardOverlay.tsx
@@ -1,31 +1,15 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import FaviconNotFound from './FaviconNotFound';
+import React from 'react';
 import { LinkType } from '@/lib/types';
+import Favicon from './Favicon';
 
-export default function LinkCardOverlay({
-	link,
-}: {
-	link: LinkType | undefined;
-}) {
+export default function LinkCardOverlay({ link }: { link: LinkType }) {
 	const title = link?.title;
 	const url = link?.url;
-	const [faviconFound, setFaviconFound] = useState(true);
 
 	return (
 		<div className='flex cursor-move justify-center rounded-xl border bg-secondary p-2 text-center shadow-sm dark:shadow-none'>
-			<div className='mr-2 flex items-center justify-center'>
-				{faviconFound ? (
-					<Image
-						height={15}
-						width={15}
-						src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${url}`}
-						alt='favicon'
-						onError={() => setFaviconFound(false)}
-					/>
-				) : (
-					<FaviconNotFound />
-				)}
+			<div className='flex items-center justify-center'>
+				<Favicon url={url} />
 			</div>
 			{title}
 		</div>
